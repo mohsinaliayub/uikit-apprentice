6dee28f0e36cc7e74905bdce6dbf9bf15486bd51
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-01-19 00:24:58 +0100

    generate random target value and display it in the alert

--- 01-bulls-eye/BullsEye/BullsEye/ViewController.swift
+++ 01-bulls-eye/BullsEye/BullsEye/ViewController.swift
@@ -11,16 +11,23 @@ class ViewController: UIViewController {
     
     /// The slider to match a randomly chosen target value.
     ///
-    /// The user drags this slider to match a target value as close as they can.
+    /// The player drags this slider to match a target value as close as they can.
     @IBOutlet weak var slider: UISlider!
     
     /// The slider's current value rounded to the nearest integer.
     var currentValue = 0
+    /// A random target value the player must try to match as close as they can.
+    ///
+    /// The random Int value is set when either a new round of the game begins
+    /// or the player starts over the game completely. This random value is between
+    /// 1 and 100 (inclusive), the slider's minimum and maximum values, respectively.
+    var targerValue = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         currentValue = lroundf(slider.value)
+        targerValue = Int.random(in: 1...100) // set a random target value between 1 and 100
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
@@ -28,7 +35,8 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)"
+        let message = "The value of the slider is: \(currentValue)" + 
+                      "\nThe target value is: \(targerValue)"
         
         let alert = UIAlertController(title: "Hello, World",
                                       message: message,
