2081f8f3a411bd3038343f83911185e67f24d15d
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-01-18 23:33:17 +0100

    read slider's value when dragged

--- 01-bulls-eye/BullsEye/BullsEye/ViewController.swift
+++ 01-bulls-eye/BullsEye/BullsEye/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of the slider is now: \(slider.value)")
+    }
+    
     @IBAction func showAlert() {
         let alert = UIAlertController(title: "Hello, World",
                                       message: "This is my first app!",
