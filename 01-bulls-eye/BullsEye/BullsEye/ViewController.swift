eab07c94b3b642c253b0707e3657266589e18c7c
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-01-19 00:10:56 +0100

    fix: read slider's initial value at app startup

--- 01-bulls-eye/BullsEye/BullsEye/ViewController.swift
+++ 01-bulls-eye/BullsEye/BullsEye/ViewController.swift
@@ -9,12 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    /// The slider to match a randomly chosen target value.
+    ///
+    /// The user drags this slider to match a target value as close as they can.
+    @IBOutlet weak var slider: UISlider!
+    
     /// The slider's current value rounded to the nearest integer.
     var currentValue = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        currentValue = lroundf(slider.value)
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
