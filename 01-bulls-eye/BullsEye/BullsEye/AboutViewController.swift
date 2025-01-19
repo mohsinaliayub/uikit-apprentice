ebed52660c687e3ab6f8e9f3e0b252b7eecabb38
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-01-20 00:40:56 +0100

    add about scene and accompanying view controller

---
+++ 01-bulls-eye/BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Mohsin Ali Ayub on 20.01.25.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
