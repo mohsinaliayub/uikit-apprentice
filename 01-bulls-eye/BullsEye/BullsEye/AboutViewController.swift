1d35b561e2bb5b8029e88c864733c9f42d609680
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-01-20 00:47:03 +0100

    dismiss about screen when close button is tapped

--- 01-bulls-eye/BullsEye/BullsEye/AboutViewController.swift
+++ 01-bulls-eye/BullsEye/BullsEye/AboutViewController.swift
@@ -13,4 +13,8 @@ class AboutViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func close() {
+        dismiss(animated: true)
+    }
 }
