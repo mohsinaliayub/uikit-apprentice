9269dbe6babb5a9e49fdec338f8db06095d16d00
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-01-20 01:03:14 +0100

    - customize the appearance of about screen - load and display game's info within an embedded web view

--- 01-bulls-eye/BullsEye/BullsEye/AboutViewController.swift
+++ 01-bulls-eye/BullsEye/BullsEye/AboutViewController.swift
@@ -6,15 +6,30 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
     
+    /// The web view to display game information.
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Load and display game info from .html file.
+        loadHTMLPage()
     }
     
     @IBAction func close() {
         dismiss(animated: true)
+    }
+    
+    /// Loads the HTML content from a bundled .html file in `webView`.
+    func loadHTMLPage() {
+        // Find the URL for the bundled .html file.
+        if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html") {
+            // Create a request and load it in our webView object.
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
 }
