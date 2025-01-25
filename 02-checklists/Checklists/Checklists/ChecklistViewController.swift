d56ae18a0ca19c180308daca51d6fabb2793c275
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-01-25 23:22:59 +0100

    display five rows in table view

--- 02-checklists/Checklists/Checklists/ChecklistViewController.swift
+++ 02-checklists/Checklists/Checklists/ChecklistViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
-
+    
+    // Properties
+    private let cellIdentifier = "ChecklistItem"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // MARK: - Table View Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        return cell
+    }
 }
 
