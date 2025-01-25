eec9a7da6318f9aa7be5251ede23b05fcfa2ff50
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-01-25 23:31:54 +0100

    provide different data for each row of table view

--- 02-checklists/Checklists/Checklists/ChecklistViewController.swift
+++ 02-checklists/Checklists/Checklists/ChecklistViewController.swift
@@ -24,6 +24,21 @@ class ChecklistViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row == 2 {
+            label.text = "Learn iOS development"
+        } else if indexPath.row == 3 {
+            label.text = "Soccer practice"
+        } else if indexPath.row == 4 {
+            label.text = "Eat ice cream"
+        }
+        
         return cell
     }
 }
