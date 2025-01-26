3ec07515ff93c70b75cf44b8bf6cea91cb27c295
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-01-26 21:23:18 +0100

    polish up text field's user experience

--- 02-checklists/Checklists/Checklists/AddItemViewController.swift
+++ 02-checklists/Checklists/Checklists/AddItemViewController.swift
@@ -19,6 +19,11 @@ class AddItemViewController: UITableViewController {
         super.viewDidLoad()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Open a keyboard before the view appears.
+        textField.becomeFirstResponder()
+    }
     
     // MARK: - Actions
     
