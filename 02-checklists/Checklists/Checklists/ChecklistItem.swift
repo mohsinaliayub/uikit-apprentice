5c819a38cbea3ba8350f536ce60b0ec497ed13a2
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-01-26 14:19:30 +0100

    rename title->text in ChecklistItem

--- 02-checklists/Checklists/Checklists/ChecklistItem.swift
+++ 02-checklists/Checklists/Checklists/ChecklistItem.swift
@@ -9,27 +9,27 @@ import Foundation
 
 /// A simple to-do item to be performed by the user.
 ///
-/// You can and should specify a unique title for your to-do item to be easily identified among others.
+/// You can and should specify a unique description for your to-do item to be easily identified among others.
 /// The to-do item can be set as complete/uncomplete.
 class ChecklistItem {
-    /// A name to identify a to-do item. Please specify a unique name to easily identify the to-do item.
-    var title: String
+    /// A description to identify a to-do item. Please specify a unique description to easily identify the to-do item.
+    var text: String
     /// The state of the to-do item, whether it is completed or not.
     var checked: Bool
     
-    /// Initializes a `ChecklistItem` object with both ``title`` and ``checked`` states.
+    /// Initializes a `ChecklistItem` object with both ``text`` and ``checked`` states.
     /// - Parameters:
-    ///   - title: The unique title to identify a to-do item.
+    ///   - text: A unique description to identify a to-do item.
     ///   - checked: The state of the to-do item: completed or not.
-    init(title: String, checked: Bool) {
-        self.title = title
+    init(text: String, checked: Bool) {
+        self.text = text
         self.checked = checked
     }
     
     
-    /// Initializes a `ChecklistItem` object with a title and a default ``checked`` state as `false`.
-    /// - Parameter title: The unique title to identify a to-do item.
-    convenience init(title: String) {
-        self.init(title: title, checked: false)
+    /// Initializes a `ChecklistItem` object with a description and a default ``checked`` state as `false`.
+    /// - Parameter title: A unique description to identify a to-do item.
+    convenience init(text: String) {
+        self.init(text: text, checked: false)
     }
 }
