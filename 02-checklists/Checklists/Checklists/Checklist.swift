2a4e61bc9265202c1e49df1c25f7c20fd4319428
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-02-02 12:31:54 +0100

    conform our model to Equatable

--- 02-checklists/Checklists/Checklists/Checklist.swift
+++ 02-checklists/Checklists/Checklists/Checklist.swift
@@ -12,6 +12,8 @@ import Foundation
 /// Specify a unique name to easily identify a to-do list later. Each to-do list has multiple to-do
 /// items related to it.
 class Checklist {
+    /// A unique identifier for to-do list.
+    private let id = UUID()
     /// The name to identify a to-do list. Specifying a unique name will easily identify the to-do list.
     var name: String
     /// An array of to-do items.
@@ -19,5 +21,12 @@ class Checklist {
     
     init(name: String) {
         self.name = name
+    }
+}
+
+extension Checklist: Equatable {
+    static func == (lhs: Checklist, rhs: Checklist) -> Bool {
+        // Each Checklist has a unique id. If id's are same, Checklist is same.
+        lhs.id == rhs.id
     }
 }
